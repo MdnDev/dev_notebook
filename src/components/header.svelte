a41dc54d9f4c1bf04e946cd8b3b9f46611dbecdd
Wilfried Maudon <wilfried.maudonpro@gmail.com>
2024-02-08 23:58:00 +0100

    first commit

---
+++ src/components/header.svelte
@@ -0,0 +1,19 @@
+<div class="navbar bg-base-100">
+	<div class="flex-1">
+		<div class="btn btn-ghost text-xl">daisyUI</div>
+	</div>
+	<div class="flex-none">
+		<ul class="menu menu-horizontal px-1">
+			<li>Link</li>
+			<li>
+				<details>
+					<summary> Parent </summary>
+					<ul class="p-2 bg-base-100 rounded-t-none">
+						<li>Link 1</li>
+						<li>Link 2</li>
+					</ul>
+				</details>
+			</li>
+		</ul>
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <button class="bg-black text-white py-2 px-4 rounded hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-gray-500 transition duration-150 ease-in-out">
+    Click Me
+  </button>
+  <style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100);
+    }
+  </style>
